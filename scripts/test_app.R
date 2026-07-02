aee124973b4faf6eb59492348ebab80d1ded9ad9
Vik <viknesh.gopal@gmail.com>
2026-07-03 05:35:55 +0800

    watch out for highs!

--- scripts/test_app.R
+++ scripts/test_app.R
@@ -7,7 +7,7 @@ library(ROI.plugin.highs)
 
 df1 <- readRDS("data009-composition.rds")
 df_list <- extract_student_info(df1, "diversity",
-                                demographic_cols = 2, skills = NULL, 
+                                demographic_cols = 2, skills = NULL,
                                 self_formed_groups = 1)
 yaml_list <- extract_params_yaml("mdl01_input009.yml", "diversity")
 
@@ -19,5 +19,7 @@ m1 <- prepare_model(df_list, yaml_list, "diversity", w1=1.0)
 # https://cran.r-universe.dev/Rglpk/doc/manual.html
 #result <- solve_model(m1, with_ROI(solver="glpk", verbose=TRUE, tm_limit=100))
 result <- solve_model(m1, with_ROI(solver="glpk", verbose=TRUE, presolve=TRUE, tm_limit=5000))
+# highs
+result <- solve_model(m1, with_ROI(solver="highs", verbose=TRUE, presolve="on", simplex_update_limit=10, time_limit=100))
 
 assigned_groups <- assign_groups(result, "diversity", df1, yaml_list, "student_id")
